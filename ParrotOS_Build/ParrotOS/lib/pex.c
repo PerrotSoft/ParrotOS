9b488dce8aabdf3624e65f7b4e183100acfccafc
PerrotSoft <zhenja.fedorenko.kr1@gmail.com>
2026-03-14 14:07:31 +0000

    Auto-build #40

--- ParrotOS_Build/ParrotOS/lib/pex.c
+++ ParrotOS_Build/ParrotOS/lib/pex.c
@@ -14,13 +14,21 @@ INT32 FindFreeTaskSlot(VOID) {
 }
 CHAR16* f = L"hi";
 CHAR16* t(){
-    return f;
+    return L"bsadhjdfsj";
 }
+struct g {
+    INT32 id;
+    CHAR16* (*HI)(VOID);
+    CHAR16* f;
+};
 EFI_STATUS LoadAndStartPex(CHAR16* Path) {
     EFI_STATUS Status;
     EC16 file;
-    
+    struct g f1;
+    f1.HI = t;
+    f1.f = f;
     INT32 id = FindFreeTaskSlot();
+    f1.id = id;
     if (id == -1) return EFI_OUT_OF_RESOURCES;
 
     Status = ReadFileByPath(Path, &file);
@@ -28,7 +36,7 @@ EFI_STATUS LoadAndStartPex(CHAR16* Path) {
 
     // Используем L"PEX", так как UEFI работает с CHAR16.
     // Если передать "PEX", адрес будет верным, но данные — неверно интерпретированы.
-    Status = task_create_with_arg(id, (VOID (*)(VOID*))file.Message, &t);
+    Status = task_create_with_arg(id, (VOID (*)(VOID*))file.Message, &f1);
     
     if (EFI_ERROR(Status)) {
         gBS->FreePool(file.Message); 
